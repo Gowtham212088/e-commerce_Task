e2d66edc2d41ad5db55839de0a210ebd4771b540
GOWTHAM KUMAR V <vgowtham.kumar8@gmail.com>
2022-07-22 11:05:31 +0530

    completion of signUp page

--- src/Home Page/Navbar.js
+++ src/Home Page/Navbar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
-import Badge from '@mui/material/Badge';
+import Badge from "@mui/material/Badge";
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import AddBusinessIcon from '@mui/icons-material/AddBusiness';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-transparent border-bottom border-3 nav-border fw-bold">
+    <nav className="navbar navbar-expand-lg bg-transparent nav-border fw-bold">
       <div className="container-fluid ">
         <a className="navbar-brand fs-3 font-clr">Navbar</a>
         <button
@@ -21,21 +23,42 @@ const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-           
             <li className="nav-item">
-              <a className="nav-link active fs-4 font-clr me-2">
-               Login
-              </a>
-            </li> 
+               
+               {/*CODE FOR BOOTSTRAP MODAL (SIGN IN)  */}
 
-            <li className="nav-item">
+            <a class="nav-link active fs-4 font-clr me-5" data-bs-toggle="modal" href="#exampleModalToggle" > Login </a>
+            </li>
+ <li className="nav-item">
+<div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-body">
+        <h3 className="d-flex  justify-content-center loginRole-text"> Choose Login Role </h3>
+      </div>
+      <div class="modal-footer d-flex justify-content-around">
+      <Button style={{backgroundColor:"#4b00a2"}} className="mt-1" variant="contained" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal"> <AdminPanelSettingsIcon className="ms-1 "/> Admin Login  </Button>
+        <Button style={{backgroundColor:"#4b00a2"}} className="mt-1" variant="contained" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal"> <AddBusinessIcon className="ms-1"/> Vendor's Login </Button>
+      </div>
+    </div>
+  </div>
+</div>
+
+{/* SignUp  */}
+
               <a className="nav-link active fs-4 font-clr me-5"> Sign Up </a>
             </li>
-           
           </ul>
-          <div className="d-flex"> 
-            <button className="btn btn-outline text-light nav-btn " type="submit">
-          <Badge badgeContent={4} color="primary">  <AddShoppingCartIcon  /> </Badge> Cart
+          <div className="d-flex">
+            <button
+              className="btn btn-outline text-light nav-btn "
+              type="submit"
+            >
+              <Badge badgeContent={4} color="primary">
+                {" "}
+                <AddShoppingCartIcon />{" "}
+              </Badge>{" "}
+              Cart
             </button>
           </div>
         </div>
--- src/Login&SignUp page/SignUp.js
+++ src/Login&SignUp page/SignUp.js
@@ -1,213 +1,280 @@
-import React,{useState} from "react";
-import add_to_cart from "../images/add_to_cart.png"
-import onlineShopping from "../images/onlineShopping.png"
-import TextField from '@mui/material/TextField';
-import MenuItem from '@mui/material/MenuItem';
+import React, { useState } from "react";
+import add_to_cart from "../images/add_to_cart.png";
+import onlineShopping from "../images/onlineShopping.png";
+import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
 const SignUp = () => {
+  
+  //! This is for District Dropdown
+  const Districs = [
+    {
+      value: "Ariyalur",
+      label: "Ariyalur",
+    },
+    {
+      value: "Chengalpattu",
+      label: "Chengalpattu",
+    },
+    {
+      value: "Chennai",
+      label: "Chennai",
+    },
+    {
+      value: "Coimbatore",
+      label: "Coimbatore",
+    },
+    {
+      value: "Cuddalore",
+      label: "Cuddalore",
+    },
+    {
+      value: "Dharmapuri",
+      label: "Dharmapuri",
+    },
+    {
+      value: "Dindigul",
+      label: "Dindigul",
+    },
+    {
+      value: "Erode",
+      label: "Erode",
+    },
+    {
+      value: "Kallakurichi",
+      label: "Kallakurichi",
+    },
+    {
+      value: "Kanchipuram",
+      label: "Kanchipuram",
+    },
+    {
+      value: "Kanyakumari",
+      label: "Kanyakumari",
+    },
+    {
+      value: "Karur",
+      label: "Karur",
+    },
+    {
+      value: "Krishnagiri",
+      label: "Krishnagiri",
+    },
+    {
+      value: "Madurai",
+      label: "Madurai",
+    },
+    {
+      value: "Nagapattinam",
+      label: "Nagapattinam",
+    },
 
-//? This is for District Dropdown
-const Districs = [
     {
-      value: 'Ariyalur',
-      label: 'Ariyalur',
+      value: "Namakkal",
+      label: "Namakkal",
     },
     {
-      value: 'Chengalpattu',
-      label: 'Chengalpattu',
+      value: "Nilgiris",
+      label: "Nilgiris",
     },
     {
-      value: 'Chennai',
-      label: 'Chennai',
+      value: "Perambalur",
+      label: "Perambalur",
     },
     {
-      value: 'Coimbatore',
-      label: 'Coimbatore',
+      value: "Pudukkottai",
+      label: "Pudukkottai",
     },
     {
-      value: 'Cuddalore',
-      label: 'Cuddalore',
+      value: "Ramanathapuram",
+      label: "Ramanathapuram",
     },
     {
-      value: 'Dharmapuri',
-      label: 'Dharmapuri',
+      value: "Ranipet",
+      label: "Ranipet",
     },
     {
-      value: 'Dindigul',
-      label: 'Dindigul',
+      value: "Salem",
+      label: "Salem",
     },
     {
-      value: 'Erode',
-      label: 'Erode',
+      value: "Sivaganga",
+      label: "Sivaganga",
     },
     {
-      value: 'Kallakurichi',
-      label: 'Kallakurichi',
+      value: "Tenkasi",
+      label: "Tenkasi",
     },
     {
-      value: 'Kanchipuram',
-      label: 'Kanchipuram',
+      value: "Thanjavur",
+      label: "Thanjavur",
     },
     {
-      value: 'Kanyakumari',
-      label: 'Kanyakumari',
+      value: "Theni",
+      label: "Theni",
     },
     {
-      value: 'Karur',
-      label: 'Karur',
+      value: "Thoothukudi",
+      label: "Thoothukudi",
     },
     {
-      value: 'Krishnagiri',
-      label: 'Krishnagiri',
+      value: "Tiruchirappalli",
+      label: "Tiruchirappalli",
     },
     {
-      value: 'Madurai',
-      label: 'Madurai',
+      value: "Tirunelveli",
+      label: "Tirunelveli",
     },
     {
-      value: 'Nagapattinam',
-      label: 'Nagapattinam',
-    },
-
-    {
-      value: 'Namakkal',
-      label: 'Namakkal',
-    },    {
-      value: 'Nilgiris',
-      label: 'Nilgiris',
-    },    {
-      value: 'Perambalur',
-      label: 'Perambalur',
-    },    {
-      value: 'Pudukkottai',
-      label: 'Pudukkottai',
-    },    {
-      value: 'Ramanathapuram',
-      label: 'Ramanathapuram',
-    },    {
-      value: 'Ranipet',
-      label: 'Ranipet',
-    },    {
-      value: 'Salem',
-      label: 'Salem',
-    },    {
-      value: 'Sivaganga',
-      label: 'Sivaganga',
-    },    {
-      value: 'Tenkasi',
-      label: 'Tenkasi',
-    },    {
-      value: 'Thanjavur',
-      label: 'Thanjavur',
-    },    {
-      value: 'Theni',
-      label: 'Theni',
-    },    {
-      value: 'Thoothukudi',
-      label: 'Thoothukudi',
-    },    {
-      value: 'Tiruchirappalli',
-      label: 'Tiruchirappalli',
-    },    {
-      value: 'Tirunelveli',
-      label: 'Tirunelveli',
-    },    {
-      value: 'Tirupathur',
-      label: 'Tirupathur',
-    },    {
-      value: 'Tiruppur',
-      label: 'Tiruppur',
-    },    {
-      value: 'Tiruvallur',
-      label: 'Tiruvallur',
-    },    {
-      value: 'Tiruvannamalai',
-      label: 'Tiruvannamalai',
-    },{
-      value: 'Tiruvarur',
-      label: 'Tiruvarur',
+      value: "Tirupathur",
+      label: "Tirupathur",
     },
     {
-      value: 'Vellore',
-      label: 'Vellore',
+      value: "Tiruppur",
+      label: "Tiruppur",
     },
     {
-      value: 'Viluppuram',
-      label: 'Viluppuram',
+      value: "Tiruvallur",
+      label: "Tiruvallur",
     },
     {
-      value: 'Viluppuram',
-      label: 'Viluppuram',
+      value: "Tiruvannamalai",
+      label: "Tiruvannamalai",
     },
     {
-      value: 'Virudhunagar',
-      label: 'Virudhunagar',
+      value: "Tiruvarur",
+      label: "Tiruvarur",
+    },
+    {
+      value: "Vellore",
+      label: "Vellore",
+    },
+    {
+      value: "Viluppuram",
+      label: "Viluppuram",
+    },
+    {
+      value: "Viluppuram",
+      label: "Viluppuram",
+    },
+    {
+      value: "Virudhunagar",
+      label: "Virudhunagar",
     },
   ];
-//? This is for District Dropdown
+
+  //! This is for District Dropdown
+
   const Role = [
     {
-      value:'Admin',
-      label:'Admin'
+      value: "Admin",
+      label: "Admin",
     },
     {
-      value:'Vendors',
-      label:'Vendors'
-    }
+      value: "Vendors",
+      label: "Vendors",
+    },
   ];
+//! State Management 
+  const [District, setDistrict] = useState("Chennai");
 
-  const [District, setDistrict] = useState('Chennai');
+  const [role, setRole] = useState("");
 
-  const [role,setRole]= useState("")
+//! Change Handlers
 
   const handleChange = (event) => {
     setDistrict(event.target.value);
-    setRole(event.target.value)
+    setRole(event.target.value);
   };
-console.log({District:District,role:role});
+  console.log({ District: District, role: role });
   return (
     <div className="container-fluid signUp-parent mt-5 justify-content-center">
-             <div className="row mt-5 h-70 justify-content-center signUp-child1">
-              <div className="col-sm-6 h-100 col-md-6 col-lg-6 col1">
-                    {/* <img className="img" src="http://unblast.com/wp-content/uploads/2020/04/Online-Shopping-Illustration.jpg" width="500px" /> */}
-              </div>
+      <div className="row mt-5 h-70 justify-content-center signUp-child1">
+        <div className="col-sm-6 col-md-6 col-lg-6 col1">
+          <h2 className="signUp-text d-flex justify-content-center ">
+            {" "}
+            Sign Up{" "}
+          </h2>
+        </div>
 
-              <div className="col-sm-4 h-100 col-md-4 col-lg-4 d-flex flex-column col2">
-               <TextField type="text" name="username" className="mt-4" id="outlined-basic" label="Name" variant="outlined"/>
-               <TextField type="email" name="email" className="mt-4" id="outlined-basic" label="E-mail" variant="outlined"/>
-               <TextField type="password" name="password"  className="mt-4" id="outlined-basic" label="Password" variant="outlined"/>
-               <TextField className="mt-4"
-          id="outlined-select-currency"
-          select
-          label="District"
-          value={District}
-          onChange={handleChange}
-          helperText="Please select your currency"
-        >
-          {Districs.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
+        <div className="col-sm-4 col-md-4 col-lg-4 d-flex flex-column col2">
+          <h3 className="Add-user d-flex justify-content-center mb-3"> Add user </h3>
+          <TextField
+            type="text"
+            name="username"
+            className="mt-3"
+            id="outlined-basic"
+            label="Name"
+            variant="outlined"
+          />
+          <TextField
+            type="email"
+            name="email"
+            className="mt-3"
+            id="outlined-basic"
+            label="E-mail"
+            variant="outlined"
+          />
+          <TextField
+            type="password"
+            name="password"
+            className="mt-3"
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+          />
+          <TextField
+            className="mt-3"
+            id="outlined-select-currency"
+            select
+            label="District"
+            value={District}
+            onChange={handleChange}
+            helperText="Please select your currency"
+          >
+            {Districs.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
 
-        <TextField className="mt-4"
-          id="outlined-select-currency"
-          select
-          label="Role"
-          value={role}
-          onChange={handleChange}
-          helperText="Please select your Role"
-        >
-          {Role.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>        
-<TextField type="file" />
-               </div>
-
-             </div>
+          <TextField
+            className="mt-3"
+            id="outlined-select-currency"
+            select
+            label="Role"
+            value={role}
+            onChange={handleChange}
+            helperText="Please select your Role"
+          >
+            {Role.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            type="url"
+            name="Image_Url"
+            className="mt-3"
+            id="outlined-basic"
+            label="Image_Url"
+            variant="outlined"
+          />
+          <div className="mt-2 forgot-password d-flex justify-content-center">
+            <a> Forgot Password ? </a>{" "}
+          </div>
+          <Button
+            className="mt-3"
+            variant="contained"
+            style={{ backgroundColor: "#4b00a2" }}
+          >
+            {" "}
+            Sign Up{" "}
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
