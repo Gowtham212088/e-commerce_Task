27586f659e380c51d1f98e41f0d61c6569833f2c
GOWTHAM KUMAR V <89139024+Gowtham212088@users.noreply.github.com>
2023-03-24 01:39:45 +0530

    Update API.js

--- src/data/API.js
+++ src/data/API.js
@@ -3,4 +3,8 @@ import React from "react";
 // export const Api = "http://localhost:5000";
 
 
-export const Api = "https://ecremmoc-e.herokuapp.com";
+// export const Api = "https://ecremmoc-e.herokuapp.com";
+
+
+
+export const Api = 'https://e-commerce-backend-eta.vercel.app'
